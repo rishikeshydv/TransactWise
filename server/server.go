4af7a2f13f8ff49024623968522aee70c49ffbbc
rishikeshydv <rishikeshadh4@gmail.com>
2023-08-29 18:19:15 -0400

    auth added

---
+++ server/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	//   "errors"
+	"fmt"
+	"io"
+	"net/http"
+	// "os"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website! \n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
